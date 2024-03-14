9521286ee73d7ac4c148a6be42226cc3915d46d3
UneYasuaki <une.yasuaki@gmail.com>
2024-03-14 14:06:53 +0900

    スタート画面からメイン画面への移動作成

---
+++ app/src/main/java/une/yasuaki/quiz/ResultActivity.java
@@ -0,0 +1,13 @@
+package une.yasuaki.quiz;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
--- app/src/main/java/une/yasuaki/quiz/StartActivity.java
+++ app/src/main/java/une/yasuaki/quiz/StartActivity.java
@@ -2,7 +2,44 @@ package une.yasuaki.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class StartActivity {
+import java.util.ArrayList;
+
+public class StartActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        //リスト項目をArray Listとして準備
+        final ArrayList<String> data=new ArrayList<>();
+        data.add("問題ジャンル1");
+        data.add("問題ジャンル2");
+
+        //配列アダプターを作成&ListViewに登録
+        ListView list= findViewById(R.id.list);
+        list.setAdapter(new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_single_choice,data
+        ));
+
+        list.setOnItemClickListener((adapterView, view, i, l) -> {
+            Toast.makeText(StartActivity.this, String.format("選択したのは%sです",((TextView)view).getText()), Toast.LENGTH_LONG).show();
+        });
+
+        //スタートボタンクリック時に呼び出されるイベントリスナー
+        Button btnStart=findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(view -> {
+            // MainActivityへのインテントを作成
+            Intent i=new Intent(StartActivity.this,MainActivity.class);
+            startActivity(i);
+        });
+    }
 }
