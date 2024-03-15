5a9e0c97be678c9f8627be9000f1b0af5e079570
UneYasuaki <une.yasuaki@gmail.com>
2024-03-15 14:03:58 +0900

    4択クイズベース完成

--- app/src/main/java/une/yasuaki/quiz/MainActivity.java
+++ app/src/main/java/une/yasuaki/quiz/MainActivity.java
@@ -2,7 +2,12 @@ package une.yasuaki.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,7 +15,7 @@ import java.util.Collections;
 import java.util.Random;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     //テキストビュー宣言
     private TextView countLabel,questionLabel;
@@ -18,24 +23,137 @@ public class MainActivity extends AppCompatActivity {
     //ボタン宣言
     private Button btna1,btna2,btna3,btna4;
 
-    //正解宣言（？）
-    private int rightAnswer;
+    //正解を入れるための宣言
+    private String rightAnswer;
 
-    //正解数宣言
+    //正解数をカウントするための宣言
     private int rightAnswerCount;
 
     //クイズ問題数カウント宣言
-    private int quizCount;
+    private int quizCount=1;
 
-    //クイズ問題をデータの大きさに応じて取り出す
-    //for(int i=0;i<quizData.length; i++){
-        //問題リストの宣言
-        ArrayList<ArrayList<String>> tmpArray = new ArrayList<>();
-    //}
+    //
+    static final private int QUIZ_COUNT = 10;
+
+    ArrayList<ArrayList<String>> quizArray = new ArrayList<>();
+
+    String[][] quizData = {
+            // {"都道府県名", "正解", "選択肢１", "選択肢２", "選択肢３"}
+            {"北海道", "札幌市", "長崎市", "福島市", "前橋市"},
+            {"青森県", "青森市", "広島市", "甲府市", "岡山市"},
+            {"岩手県", "盛岡市","大分市", "秋田市", "福岡市"},
+            {"宮城県", "仙台市", "水戸市", "岐阜市", "福井市"},
+            {"秋田県", "秋田市","横浜市", "鳥取市", "仙台市"},
+            {"山形県", "山形市","青森市", "山口市", "奈良市"},
+            {"福島県", "福島市", "盛岡市", "新宿区", "京都市"},
+            {"茨城県", "水戸市", "金沢市", "名古屋市", "奈良市"},
+            {"栃木県", "宇都宮市", "札幌市", "岡山市", "奈良市"},
+            {"群馬県", "前橋市", "福岡市", "松江市", "福井市"},
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countLabel = findViewById(R.id.countLabel);
+        questionLabel = findViewById(R.id.questionLabel);
+        btna1 = findViewById(R.id.btna1);
+        btna2 = findViewById(R.id.btna2);
+        btna3 = findViewById(R.id.btna3);
+        btna4 = findViewById(R.id.btna4);
+
+        btna1.setOnClickListener(this);
+        btna2.setOnClickListener(this);
+        btna3.setOnClickListener(this);
+        btna4.setOnClickListener(this);
+
+        // quizDataからクイズ出題用のquizArrayを作成する
+        for (String[] quizDatum : quizData) {
+
+            // 新しいArrayListを準備
+            ArrayList<String> tmpArray = new ArrayList<>();
+
+            // クイズデータを追加
+            tmpArray.add(quizDatum[0]);  // 都道府県名
+            tmpArray.add(quizDatum[1]);  // 正解
+            tmpArray.add(quizDatum[2]);  // 選択肢１
+            tmpArray.add(quizDatum[3]);  // 選択肢２
+            tmpArray.add(quizDatum[4]);  // 選択肢３
+
+            // tmpArrayをquizArrayに追加する
+            quizArray.add(tmpArray);
+        }
+
+        showNextQuiz();
+    }
+
+    public void showNextQuiz() {
+        // クイズカウントラベルを更新
+        countLabel.setText(getString(R.string.count_label, quizCount));
+
+        // ランダムな数字を取得
+        Random random = new Random();
+        int randomNum = random.nextInt(quizArray.size());
+
+        // randomNumを使って、quizArrayからクイズを一つ取り出す
+        ArrayList<String> quiz = quizArray.get(randomNum);
+
+        // 問題文（都道府県名）を表示
+        questionLabel.setText(quiz.get(0));
+
+        // 正解をrightAnswerにセット
+        rightAnswer = quiz.get(1);
+
+        // クイズ配列から問題文（都道府県名）を削除
+        quiz.remove(0);
+
+        // 正解と選択肢３つをシャッフル
+        Collections.shuffle(quiz);
+
+        // 解答ボタンに正解と選択肢３つを表示
+        btna1.setText(quiz.get(0));
+        btna2.setText(quiz.get(1));
+        btna3.setText(quiz.get(2));
+        btna4.setText(quiz.get(3));
+
+        // このクイズをquizArrayから削除
+        quizArray.remove(randomNum);
+    }
+
+    @Override
+    public void onClick(View view) {
+        // どの解答ボタンが押されたか
+        Button answerBtn = findViewById(view.getId());
+        String btnText = answerBtn.getText().toString();
+
+        String alertTitle;
+        if (btnText.equals(rightAnswer)) {
+            alertTitle = "正解!";
+            rightAnswerCount++;
+        } else {
+            alertTitle = "不正解...";
+        }
+
+        // ダイアログを作成
+        new MaterialAlertDialogBuilder(this)
+                .setTitle(alertTitle)
+                .setMessage("答え : " + rightAnswer)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (quizCount == QUIZ_COUNT) {
+                            // 結果画面へ移動
+                            Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                            intent.putExtra("RIGHT_ANSWER_COUNT", rightAnswerCount);
+                            startActivity(intent);
+                        } else {
+                            quizCount++;
+                            showNextQuiz();
+                        }
+                    }
+                })
+                .setCancelable(false)
+                .show();
     }
 }
--- app/src/main/java/une/yasuaki/quiz/ResultActivity.java
+++ app/src/main/java/une/yasuaki/quiz/ResultActivity.java
@@ -1,7 +1,11 @@
 package une.yasuaki.quiz;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultActivity extends AppCompatActivity {
@@ -9,5 +13,35 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        TextView resultLabel = findViewById(R.id.resultLabel);
+        TextView totalScoreLabel = findViewById(R.id.totalScoreLabel);
+
+        // 正解数をインテントから取得
+        int score = getIntent().getIntExtra("RIGHT_ANSWER_COUNT", 0);
+
+        // (SharedPreferencesは端末保存、MODE_PRIVATEは自アプリからのみ変更可)Android端末のトータルスコアの読み出し
+        SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
+        int totalScore = prefs.getInt("TOTAL_SCORE", 0);
+
+        // トータルスコアに今回のスコアを加算
+        totalScore += score;
+
+        // TextViewに表示する
+        resultLabel.setText(getString(R.string.result_score, score));
+        totalScoreLabel.setText(getString(R.string.result_total_score, totalScore));
+
+        // トータルスコアを保存
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("TOTAL_SCORE", totalScore);
+        editor.apply();
+
+        //戻るボタン処理
+        Button resultReturn=findViewById(R.id.resultReturn);
+        resultReturn.setOnClickListener(view -> {
+            Intent intent = new Intent(ResultActivity.this, StartActivity.class);
+            //intent.putExtra("",);
+            startActivity(intent);
+        });
     }
 }
